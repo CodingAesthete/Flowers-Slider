255357a27853649f8030439f9e851ec7e9df1314
PostPunkAesthete <harry_ollI@abv.bg>
2023-05-15 10:39:30 +0300

    Upgate transition

--- script.js
+++ script.js
@@ -7,37 +7,46 @@ $(window).on('load', function () {
 
   createSound();
 
-  var imgCount = $('#slider ul li').length;
-  var imgWidth = $('#slider ul li').first().width();
-  var totalWidth = (imgWidth * imgCount) + 'px';
+  const imgCount = $('#slider ul li').length;
+  const imgWidth = $('#slider ul li').first().width();
+  const totalWidth = (imgWidth * imgCount) + 'px';
 
-  var leftPosition = 0;
-  var counter = 0;
+  let leftPosition = 0;
+  let counter = 0;
 
   $('#slider ul').css("width", totalWidth);
 
-  var timer = setInterval(slider, 2000);
+  let timer = setInterval(slider, 2000);
+  $('#slider ul li').mouseover(function () {
+    setTimeout(function () {
+      clearInterval(timer);
+      }); 
+    });
 
-  function slider() {
-    counter++;
-
-    if (counter == imgCount) {
-      $('#slider ul').clone().appendTo('#slider');
-      $('#slider ul').last().css('left', imgWidth + 'px');
-
-      leftPosition = `-${totalWidth}`
-
-      $('#slider ul').last().animate({ left: 0 }, 700, "easeInQuad");
-      $('#slider ul').first().animate({ left: leftPosition }, 700, "easeInQuad", function () {
-        $('#slider ul').first().remove();
+    $('#slider ul li').mouseout(function () {
+      timer=setInterval(slider, 2000);
       });
-
+      
+  function slider() {
+  counter++;
+  
+    if (counter == imgCount) {
+    $('#slider ul').clone().appendTo('#slider');
+    $('#slider ul').last().css('left', imgWidth + 'px');
+    
+      leftPosition = `-${totalWidth}`
+    
+      $('#slider ul').last().animate({ left: 0 }, 700, "easeInQuad");
+    $('#slider ul').first().animate({ left: leftPosition }, 700, "easeInQuad", function () {
+      $('#slider ul').first().remove();
+    });
+  
       counter = 0;
-    }
+}
     else {
       leftPosition = `-${counter * imgWidth}px`;
-      $("#slider ul").animate({ left: leftPosition }, 700, "easeInQuad");
-    }
+  $("#slider ul").animate({ left: leftPosition }, 700, "easeInQuad");
+}
   };
 
   // $('#previous').click(function () {
@@ -84,51 +93,51 @@ function createRain() {
     }
 
     update() {
-      this.y += this.speed;
-      if (this.y > canvas.height) {
-        this.y = Math.random() * -50;
-      }
-      ctx.beginPath();
-      ctx.moveTo(this.x, this.y);
-      ctx.lineTo(this.x, this.y + this.length);
-      ctx.strokeStyle = this.color;
-      ctx.lineWidth = 2;
-      ctx.stroke();
+  this.y += this.speed;
+  if (this.y > canvas.height) {
+    this.y = Math.random() * -50;
+  }
+ctx.beginPath();
+ctx.moveTo(this.x, this.y);
+ctx.lineTo(this.x, this.y + this.length);
+ctx.strokeStyle = this.color;
+ctx.lineWidth = 2;
+ctx.stroke();
     }
   }
 
   for (let i = 0; i < 100; i++) {
-    const x = Math.random() * canvas.width;
-    const y = Math.random() * canvas.height;
-    const speed = Math.random() * 5 + 10;
-    const drop = new Raindrop(x, y, speed);
-    drops.push(drop);
-  }
+  const x = Math.random() * canvas.width;
+  const y = Math.random() * canvas.height;
+  const speed = Math.random() * 5 + 10;
+  const drop = new Raindrop(x, y, speed);
+  drops.push(drop);
+}
 
   function animate() {
-    requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < drops.length; i++) {
-      drops[i].update();
-    }
-  };
-
-  animate();
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < drops.length; i++) {
+    drops[i].update();
+  }
 };
 
+  animate();
+}; 
+
 function createSound(){
-  var audio = document.getElementById("audio");
+  var audio = document .getElementById("audio");
   var soundIcon = document.getElementById("sound-icon");
   soundIcon.src = "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-volume-mute-512.png";
   $('#audio').prop('volume', 0.08);
-
-  soundIcon.addEventListener("click", function () {
-    if (audio.paused) {
-      audio.play();
-      soundIcon.src = "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-volume-high-512.png";
+  
+    soundIcon.addEventListener("click", function () {
+      if (audio.paused) {
+        audio.play();
+        soundIcon.src = "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-volume-high-512.png";
     } else {
-      audio.pause();
-      soundIcon.src = "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-volume-mute-512.png";
-    }
-  });
+        audio.pause();
+        soundIcon.src = "https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-volume-mute-512.png";
+      }
+    });
 }
